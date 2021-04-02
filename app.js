d380a1e9fcdc1003a16fd5df93b4060c1d10e68e
overflowedBuffer <mohammad.3168@yahoo.com>
2021-04-02 15:39:01 +0430

    reachable fn

--- app.js
+++ app.js
@@ -12,37 +12,56 @@ const start = 'q0'
 
 const alphabet = ['a', 'b']
 
-const nextNodes = (node, alphabet) => {
+const nextNodes = (table,node, alphabet) => {
     const nexts = []
     for (const char of alphabet) {
         if (node[char]) {
-            nexts.push(node[char])
+            nexts.push(table[node[char]])
         }
     }
+    console.log({nexts});
     return nexts
 }
 
 const nextSet = (table, nodes) => {
     const next = []
+    // console.log({nodes});
     for (const node of nodes) {
         /// inja momkene ride bashim
-        next.push(...nextNodes(node, alphabet))
+        next.push(...nextNodes(table,node, alphabet))
     }
     return next
 }
-
-console.log(nextSet(transition,[transition['q0'], transition['q1']]))
-
-const getUnusedStates = () => {
-
-}
-
 const union = (setA, setB) => {
     let _union = new Set(setA)
     for (let elem of setB) {
         _union.add(elem)
     }
     return _union
+}
+
+const reachable = (table , start) =>{
+    let reachableSet = new Set()
+    reachableSet.add(start)
+    let isThereNewReachableNodes = true
+    
+    while (isThereNewReachableNodes){
+        let reachableCount = reachableSet.size
+
+        for (let item of reachableSet)
+        {
+            // console.log([item]);
+            reachableSet = union(reachableSet , nextSet(table,[item]))
+        }
+
+        isThereNewReachableNodes = (reachableCount < reachableSet.size)
+    }
+    console.log({reachableSet })
+}
+// console.log(nextSet(transition,[transition['q0'], transition['q1']]))
+reachable(transition , transition[start])
+const getUnusedStates = () => {
+
 }
 
 // Remove the unrechable states
